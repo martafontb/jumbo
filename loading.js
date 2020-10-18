b1a65ec8e26f345d508ea7af935818c3d84ba8d8
Marta Font <martafontb@gmail.com>
2020-10-18 11:19:50 +0200

    added rafi's feedback

--- loading.js
+++ loading.js
@@ -1,3 +1,23 @@
+//image loading
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0.10) {
+      entry.target.classList.add("animate")
+    } else {
+      entry.target.classList.remove("animate")
+    }
+  })
+}, {
+  threshold: [0, 0.10, 0.9, 1]
+})
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+  observer.observe(image)
+})
+
+
 // collapsible
 var coll = document.getElementsByClassName("collapsible");
 var i;
