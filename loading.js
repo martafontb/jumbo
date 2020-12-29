de611127fe0612496ce72a4c0bff46408fd1fdcb
Marta Font <martafontb@gmail.com>
2020-12-29 17:33:55 +0100

    changed hero image

--- loading.js
+++ loading.js
@@ -1,22 +1,11 @@
-//image loading
+var lazy = function lazy() {
+  document.addEventListener('lazyloaded', function (e)  {
+    e.target.parentNode.classList.add('image-loaded');
+    e.target.parentNode.classList.remove('loading');
+  });
+}
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0.10) {
-      entry.target.classList.add("animate")
-    } else {
-      entry.target.classList.remove("animate")
-    }
-  })
-}, {
-  threshold: [0, 0.10, 0.9, 1]
-})
-
-const images = document.querySelectorAll('img');
-images.forEach(image => {
-  observer.observe(image)
-})
-
+lazy();
 
 // collapsible
 var coll = document.getElementsByClassName("collapsible");
